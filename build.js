c4eaedc94055c6b3c9311e9faa70c16e34776e53
Iwakami Sho <iwaue.sho@gmail.com>
2025-11-21 22:20:47 +0900

    updatte

--- build.js
+++ build.js
@@ -10,15 +10,39 @@ async function main() {
   });
 
   const works = result.results.map(page => {
-    return {
-      title: page.properties.Title.title[0]?.plain_text || "No Title",
-      date: page.properties.Date.date?.start || "",
-      description: page.properties.Description.rich_text[0]?.plain_text || "",
-      image: page.properties.Image.files[0]?.file?.url || ""
-    };
+    const props = page.properties;
+
+    // 安全に Title を取得
+    const title =
+      props.Title?.title?.[0]?.plain_text || "No Title";
+
+    // 安全に Date を取得
+    const date =
+      props.Date?.date?.start || "";
+
+    // 安全に Description を取得
+    const description =
+      props.Description?.rich_text?.[0]?.plain_text || "";
+
+    // Notion image プロパティの file/external 両対応
+    let image = "";
+    const fileObj = props.Image?.files?.[0];
+    if (fileObj) {
+      if (fileObj.file?.url) {
+        image = fileObj.file.url;
+      } else if (fileObj.external?.url) {
+        image = fileObj.external.url;
+      }
+    }
+
+    return { title, date, description, image };
   });
 
+  // public ディレクトリへ JSON を出力
   fs.writeFileSync("./public/works.json", JSON.stringify(works, null, 2));
 }
 
-main();
+main().catch(err => {
+  console.error("Build error:", err);
+  process.exit(1);
+});
